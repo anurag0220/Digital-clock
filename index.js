269fc1d7147ab57229bc7ba6d6bf1a579d2f8b71
Anurag Mishra <91476245+anurag0220@users.noreply.github.com>
2022-11-13 22:08:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const hour1=document.getElementById("hour");
+const minute1=document.getElementById("min");
+const seconds1=document.getElementById("sec");
+const ampm1=document.getElementById("ampm");
+
+function updateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM"
+    if(h>12)
+    {
+        h=h-12;
+        ampm="PM";
+    }
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+    hour1.innerText=h;
+    minute1.innerText=m;
+    seconds1.innerText=s;
+    ampm1.innerText=ampm;
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+updateClock()
